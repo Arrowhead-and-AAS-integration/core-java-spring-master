e526df87fc876a12893aaf01426d690eabc579c5
andercrzz <98536868+andercrzz@users.noreply.github.com>
2025-05-25 22:24:58 +0200

    cause of publishing event failure

--- eventhandler/src/main/java/eu/arrowhead/core/eventhandler/publish/PublishEventTask.java
+++ eventhandler/src/main/java/eu/arrowhead/core/eventhandler/publish/PublishEventTask.java
@@ -66,7 +66,7 @@ public class PublishEventTask implements Runnable {
 			logger.error("Sending event to: {}", subscriptionUri.toUriString());
 			httpService.sendRequest(subscriptionUri, HttpMethod.POST, Void.class, DTOConverter.convertEventPublishRequestDTOToEventDTO(publishRequestDTO));			
 		} catch (final Throwable ex) {			
-			logger.error("PUTA MIERDA:", ex.getMessage());	
+			logger.error("PUTA MIERDA:" + ex.getCause() + " - " + ex.getMessage(), ex.getStackTrace());	
 		}
 	}
 	
